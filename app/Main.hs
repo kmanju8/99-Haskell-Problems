e0a8090e78440e0814b54f4b55dd2a2db9c105db
Keshav Manju <keshava249@gmail.com>
2023-08-10 16:39:08 +0100

    remove bits to gitignore

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "Hello, Haskell!"
+    let ans = myButLast [1,2,3,4,17]
+    print ans
+
+-- Problem 1
+myLast :: [a] -> a
+myLast [] = error "last element of empty list" 
+myLast [x] = x 
+myLast (_:xs) = myLast xs
+
+-- Problem 2
+myButLast :: [a] -> a
+myButLast [] = error "last element of empty list" 
+myButLast [_] = error "list too short"
+myButLast [x,_] = x
+myButLast (_:xs) = myButLast xs
